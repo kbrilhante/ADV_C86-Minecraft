54b2df8628a5fc053606ed9e41a201d3ac7485d7
Karina <kbrilhante@gmail.com>
2022-08-15 22:35:36 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+const canvas = new fabric.Canvas('myCanvas');
+
+var blockImageWidth = 30;
+var blockImageHeight = 30;
+
+var playerX = 10;
+var playerY = 10;
+
+var playerObject = "";
+var blockImageObject = "";
+
+function playerUpdate() {
+    fabric.Image.fromURL("player.png", function(Img){
+        playerObject = Img;
+        playerObject.scaleToWidth(150);
+        playerObject.scaleToHeight(140);
+        playerObject.set({
+            top: playerY,
+            left: playerX
+        });
+        canvas.add(playerObject);
+    });
+}
+
+function newImage(getImage) {
+    fabric.Image.fromURL(getImage, function(Img) {
+        blockImageObject = Img;
+        blockImageObject.scaleToWidth(blockImageWidth);
+        blockImageObject.scaleToHeight(blockImageHeight);
+        blockImageObject.set({
+            top: playerY,
+            left: playerX
+        });
+        canvas.add(blockImageObject)
+    });
+}
+
+playerUpdate();
+newImage("bricks.jpg")
