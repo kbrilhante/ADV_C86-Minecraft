c53bde19868f768d65c8ac51f01d360d365b73ce
Karina <kbrilhante@gmail.com>
2022-08-18 17:47:51 -0300

    Update main.js

--- main.js
+++ main.js
@@ -6,14 +6,17 @@ var blockImageHeight = 30;
 var playerX = 10;
 var playerY = 10;
 
+const playerWidth = 150;
+const playerHeight = 140;
+
 var playerObject = "";
 var blockImageObject = "";
 
 function playerUpdate() {
     fabric.Image.fromURL("player.png", function(Img){
         playerObject = Img;
-        playerObject.scaleToWidth(150);
-        playerObject.scaleToHeight(140);
+        playerObject.scaleToWidth(playerWidth);
+        playerObject.scaleToHeight(playerHeight);
         playerObject.set({
             top: playerY,
             left: playerX
